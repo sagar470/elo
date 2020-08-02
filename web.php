640cb5a8c12eba213fa3ea3ae0521df671081364
Sagar <sagarjha698@gmail.com>
2020-08-02 21:40:28 +0530

    forms and controller

--- web.php
+++ web.php
@@ -33,6 +33,14 @@ use Illuminate\Support\Facades\Route;
 
 });
     Route::get('/articles', 'ArticlesController@index');
-    Route::get('/articles/{articles}','ArticlesController@show');
+Route::post('/articles', 'ArticlesController@store');
+
+Route::get('/articles/create','ArticlesController@create');
+
+Route::get('/articles/{article}','ArticlesController@show')->name('articles.show');
+
+Route::get('/articles/{article}/edit','ArticlesController@edit');
+
+Route::put('/articles/{article}','ArticlesController@update');
 
 
